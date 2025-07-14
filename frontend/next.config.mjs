d56f18ac0f8207c81427f71abc57628d44290162
Ismartdesigns <michaelchideradickson18@gmail.com>
2025-07-14 08:59:46 +0100

    Fetch endpoint

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,15 +9,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    const BACKEND_URL = process.env.NEXT_PUBLIC_FASTAPI_URL || 'https://medplant-yjp2.onrender.com';
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${BACKEND_URL}/api/:path*`,
-      },
-    ];
-  },
+
   output: 'standalone',
 };
 
